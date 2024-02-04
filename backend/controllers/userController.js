4cee011a400e2d9e0c05eccfdcbfaddbd1a16070
Girish Nalawade <girishnalawade01@gmail.com>
2024-02-04 23:16:33 +0530

    feat :  added password reset link using email

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ const catchAsyncErrors = require("../middleware/catchAsyncError");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
 const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 // Register a User
 exports.registerUser = catchAsyncErrors( async(req,res,next) => {
@@ -110,3 +111,32 @@ exports.forgotPassword = catchAsyncErrors(async(req,res,next)=>{
         return next(new ErrorHandler(error.message, 500));
     }
 });
+
+// Reset Password 
+exports.resetPassword = catchAsyncErrors(async (req, res, next) =>{
+
+// creating token hash
+    const resetPasswordToken = crypto.createHash("sha256").update(req.params.token).digest("hex");
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() },
+    });
+
+    if(!user){
+        return next(new ErrorHandler("Reset Password Token is invalid or has been expired", 400));
+    }
+
+    if(req.body.password !== req.body.confirmPassword){
+    return next(new ErrorHandler("New password and confirm Password does not match", 400));
+    }
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordToken = undefined;
+
+    await user.save();
+
+    sendToken(user, 200, res);
+});
+
--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,12 +4,29 @@ module.exports = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 
-// Wrong mongoDb error
+// Wrong mongoDb Id error
 if(err.name === "CastError"){
     const message = `Resource not found. Invalid: $(err.path)`;
     err = new ErrorHandler(message, 400);
 }
 
+// mongoose duplicate key error 
+if(err.code === 11000){
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`
+    err = new ErrorHandler(message, 400);
+}
+
+// Wrong JWT Token error
+if(err.name === "JsonWebTokenError"){
+    const message = `Json Web Token is invalid, Please Try again `;
+    err = new ErrorHandler(message, 400);
+}
+
+// JWT expire Error
+if(err.name === "TokenExpiredError"){
+    const message = `Json Web Token is Expired, Please Try again `;
+    err = new ErrorHandler(message, 400);
+}
 
     res.status(err.statusCode).json({
         success: false,
