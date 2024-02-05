e9d3365866a7727c07bd025b3a865bf2476b6591
Girish Nalawade <girishnalawade01@gmail.com>
2024-02-05 23:41:17 +0530

    feat : created get user details , Update Change Password

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const { registerUser, loginUser, logout, forgotPassword, resetPassword } = require("../controllers/userController");
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails, updatePassword } = require("../controllers/userController");
+
+const {isAuthenticatedUser, authorizeRoles} = require("../middleware/auth")
+
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -11,5 +14,9 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser,getUserDetails);
+
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
 module.exports = router;
